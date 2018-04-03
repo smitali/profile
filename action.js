a43e6a88a06135eee0f4aa96b3e141a1c73a8e0c
Jyoti Prakash Pradhan <jyotiprakash@Jyotis-MacBook-Pro.local>
2018-04-03 23:14:01 +0530

    beautify code

--- action.js
+++ action.js
@@ -13,16 +13,16 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
 
@@ -31,31 +31,32 @@ function showSlides(n) {
 window.addEventListener('scroll', scrollFunction)
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 // portfolio-section
 
 filterSelection("all")
+
 function filterSelection(c) {
-  var items,i;
+  var items, i;
   items = document.getElementsByClassName("item");
-  
+
   for (i = 0; i < items.length; i++) {
-    if(c === 'all') {
+    if (c === 'all') {
       items[i].style.display = 'inline-block';
     } else {
-      if(items[i].classList.contains(c)) {
+      if (items[i].classList.contains(c)) {
         items[i].style.display = 'inline-block';
       } else {
         items[i].style.display = 'none';
@@ -67,18 +68,18 @@ function filterSelection(c) {
 
 // sticky page-nav-space-holder
 
- window.addEventListener('scroll', myFunction);
+window.addEventListener('scroll', myFunction);
 
-    var pagenav = document.querySelector('.page-nav-space-holder');
-    var sticky = pagenav.offsetTop;
+var pagenav = document.querySelector('.page-nav-space-holder');
+var sticky = pagenav.offsetTop;
 
-    function myFunction() {
-      if (window.pageYOffset >= sticky) {
-        pagenav.classList.add("sticky");
-      } else {
-        pagenav.classList.remove("sticky");
-      }
-    }
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    pagenav.classList.add("sticky");
+  } else {
+    pagenav.classList.remove("sticky");
+  }
+}
 
 // Used to toggle the menu on small screens when clicking on the menu button
 
@@ -92,18 +93,18 @@ function toggleFunction(event, elem) {
 
 function renderSkillPercentage() {
   var skillsElement = document.querySelectorAll('.chart-theme');
-  for ( var el = 0; el < skillsElement.length; el++) {
+  for (var el = 0; el < skillsElement.length; el++) {
     var percentage = skillsElement[el].getAttribute('data-percent');
     radialIndicator(skillsElement[el], {
-        barColor : '#00BCD4',
-        barWidth : 5,
-        initValue : percentage,
-        percentage: true,
-        fontColor: '#565454',
-        fontWeight: 80,
-        fontSize: 16
-            }); 
+      barColor: '#00BCD4',
+      barWidth: 5,
+      initValue: percentage,
+      percentage: true,
+      fontColor: '#565454',
+      fontWeight: 80,
+      fontSize: 16
+    });
   }
 }
 
-renderSkillPercentage(); 
+renderSkillPercentage();
